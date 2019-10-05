f13724e92df29edfa675c28b4e926502d5bd3c77
Nelson Sanchez <nsanchez1310@gmail.com>
2019-10-05 12:22:50 -0300

    Produto e Associação muitos para muitos

--- src/main/java/com/nsanchez1310/cursomc/CursomcApplication.java
+++ src/main/java/com/nsanchez1310/cursomc/CursomcApplication.java
@@ -8,14 +8,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nsanchez1310.cursomc.domain.Categoria;
+import com.nsanchez1310.cursomc.domain.Produto;
 import com.nsanchez1310.cursomc.repositories.CategoriaRepository;
+import com.nsanchez1310.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
-    @Autowired
+
+	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -23,11 +29,33 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		/*
+		 * Instancia os objetos preparando para gravação no BD
+		 */
+		
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
-		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		/*
+		 * Instancia a lista, montando o relacionamento many to many das tabelas
+		 */
+		// 1) Relaciona categorias com produtos
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+	
+		// 2) Relaciona categoria produtos com categorias
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));	
+		p3.getCategorias().addAll(Arrays.asList(cat1));	
+		/*
+		 * Grava no DB
+		 */
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 	}
 
