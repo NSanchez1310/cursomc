15cfc105cbfa7a3b312ec7b3ad48822e83d65eab
Nelson Sanchez <nsanchez1310@gmail.com>
2019-10-12 13:23:44 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/nsanchez1310/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.nsanchez1310.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nsanchez1310.cursomc.domain.Pedido;
+import com.nsanchez1310.cursomc.repositories.PedidoRepository;
+import com.nsanchez1310.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+
+}
