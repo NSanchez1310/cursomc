7a2c01fa8ad65481b587542cb30c3a169bfaddb2
Nelson Sanchez <nsanchez1310@gmail.com>
2019-11-03 18:47:22 -0300

    Personalizando o retorno da validação

--- src/main/java/com/nsanchez1310/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/nsanchez1310/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,7 +17,6 @@ import com.nsanchez1310.cursomc.services.exception.ObjectNotFoundException;
 public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ObjectNotFoundException.class)
-	
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
@@ -23,10 +24,21 @@ public class ResourceExceptionHandler {
 	}
 
 	@ExceptionHandler(DataIntegrityException.class)
-		
 	public ResponseEntity<StandardError> dataintegrity(DataIntegrityException e, HttpServletRequest request) {
 			
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+
+	
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+			
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		for (FieldError x: e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 
 	
