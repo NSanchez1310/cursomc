117d8c40651121213ae4539757044ace931cd540
Nelson Sanchez <nsanchez1310@gmail.com>
2019-11-12 19:42:29 -0300

    Correções:update em CategoriaService e construtor de pagamento

--- src/main/java/com/nsanchez1310/cursomc/services/ClienteService.java
+++ src/main/java/com/nsanchez1310/cursomc/services/ClienteService.java
@@ -3,7 +3,6 @@ package com.nsanchez1310.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
-import org.hibernate.boot.UnsupportedOrmXsdVersionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
