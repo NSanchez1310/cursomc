c9ef0b8d153a82671d487cbea2e1401a8d59af77
Nelson Sanchez <nsanchez1310@gmail.com>
2020-02-16 16:31:07 -0300

    Subtotal e total do Pedido

--- src/main/java/com/nsanchez1310/cursomc/domain/Pedido.java
+++ src/main/java/com/nsanchez1310/cursomc/domain/Pedido.java
@@ -88,6 +88,15 @@ public class Pedido  implements Serializable {
 		this.cliente = cliente;
 		this.setEnderecoDeEntrega(enderecoDeEntrega);
 	}
+	
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido ip: itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+		
+	}
 
 	@Override
 	public int hashCode() {
