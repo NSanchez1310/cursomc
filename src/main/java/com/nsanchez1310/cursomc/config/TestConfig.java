09fab3b91474174aa35279507006376b8c9ad177
Nelson Sanchez <nsanchez1310@gmail.com>
2020-02-24 18:56:33 -0300

    Configuracao Inicial do Spring Security

--- src/main/java/com/nsanchez1310/cursomc/config/TestConfig.java
+++ src/main/java/com/nsanchez1310/cursomc/config/TestConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.nsanchez1310.cursomc.services.DBService;
 import com.nsanchez1310.cursomc.services.EmailService;
@@ -32,5 +33,10 @@ public class TestConfig {
 	public EmailService emailService() {
 		return new MockEmailService();
 	}
+	@Bean
+    public JavaMailSenderImpl mailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        return javaMailSender;
+   }
 
 }
