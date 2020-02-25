e8d033d8f80c1c1bdc960930d33c9cbd4d2ef0c2
Nelson Sanchez <nsanchez1310@gmail.com>
2020-02-25 16:34:42 -0300

    Refres Token

---
+++ src/main/java/com/nsanchez1310/cursomc/resources/AuthResource.java
@@ -0,0 +1,31 @@
+package com.nsanchez1310.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nsanchez1310.cursomc.security.JWTUtil;
+import com.nsanchez1310.cursomc.security.UserSS;
+import com.nsanchez1310.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+	
+}
