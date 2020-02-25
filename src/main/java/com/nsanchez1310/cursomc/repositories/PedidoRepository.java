e9ffd3e2724db8f324bf4d77d3250fc2e66d9743
Nelson Sanchez <nsanchez1310@gmail.com>
2020-02-25 16:07:09 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/nsanchez1310/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/nsanchez1310/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,18 @@
 package com.nsanchez1310.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.nsanchez1310.cursomc.domain.Cliente;
 import com.nsanchez1310.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> { 
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
--- src/main/java/com/nsanchez1310/cursomc/services/PedidoService.java
+++ src/main/java/com/nsanchez1310/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.nsanchez1310.cursomc.domain.Cliente;
 import com.nsanchez1310.cursomc.domain.ItemPedido;
 import com.nsanchez1310.cursomc.domain.PagamentoComBoleto;
 import com.nsanchez1310.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.nsanchez1310.cursomc.domain.enums.EstadoPagamento;
 import com.nsanchez1310.cursomc.repositories.ItemPedidoRepository;
 import com.nsanchez1310.cursomc.repositories.PagamentoRepository;
 import com.nsanchez1310.cursomc.repositories.PedidoRepository;
+import com.nsanchez1310.cursomc.security.UserSS;
+import com.nsanchez1310.cursomc.services.exception.AuthorizationException;
 import com.nsanchez1310.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -72,4 +78,18 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		
+		UserSS user = UserService.authenticated();
+		if (user==null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
+	
+	
 }
