29da6b8e2b344fdfe649614eb26fefc5d2241414
Nelson Sanchez <nsanchez1310@gmail.com>
2020-03-01 12:17:46 -0300

    Tratando excecoes adequadamente

---
+++ src/main/java/com/nsanchez1310/cursomc/services/exception/FileException.java
@@ -0,0 +1,19 @@
+package com.nsanchez1310.cursomc.services.exception;
+
+public class FileException extends RuntimeException  {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public FileException(String msg) {
+		super(msg);
+	}
+	
+	public FileException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+	
+	
+}
