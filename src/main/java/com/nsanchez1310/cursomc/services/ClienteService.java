3f4375f799bcfb7936c43e5c5551ac492836d354
Nelson Sanchez <nsanchez1310@gmail.com>
2020-03-01 18:22:46 -0300

    Endpoint para buscar cliente por email

--- src/main/java/com/nsanchez1310/cursomc/services/ClienteService.java
+++ src/main/java/com/nsanchez1310/cursomc/services/ClienteService.java
@@ -97,6 +97,24 @@ public class ClienteService {
 		return repo.findAll();
 	}
 	
+	
+	/* busca por email
+	 * 
+	 */
+	public Cliente findByEmail(String email) {
+		
+		UserSS user = UserService.authenticated();
+		if (user==null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso Negado!");
+		}
+		
+		Cliente obj = repo.findByEmail(email);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id : " + user.getId() + " Tipo : " + Cliente.class.getName() + " / Email : " + email );
+		}
+		return obj;
+	}
+	
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
